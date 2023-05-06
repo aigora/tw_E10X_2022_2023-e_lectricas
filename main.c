d63775d178961ecf3cbc67af07977bf6ae76a5f8
Lauraantonadiaz <130753048+Lauraantonadiaz@users.noreply.github.com>
2023-05-06 21:26:25 +0200

    Añadir valores estadisticos

--- main.c
+++ main.c
@@ -23,6 +23,63 @@ menu();
 	switch(numero){
 		case 1:
 		printf("CALCULOS ESTADISTICOS\n");
+		{
+
+    int n, i; //n es el numero de valores segun el año que elijamos 
+    float num, sum = 0, media;
+
+    for (i = 1; i <= n; ++i)
+	{
+        printf("Ingrese el valor %d: ", i);
+        scanf("%f", &num);
+        sum += num;
+    }
+
+  media = sum / n;
+    printf("La media es: %.2f", media);		
+	}
+	{
+    int n, mediana, mediana2;
+    printf("Ingrese el número de elementos en el conjunto de datos: ");
+    scanf("%d", &n);
+    //para n numero de datos 
+
+    // Leer los datos y calcular la mediana
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &mediana2);
+        if (i == n / 2) {  // Si se ha leído la mitad de los datos, almacenar la mediana y salir del bucle
+            mediana = mediana2;
+            if (n % 2 == 0) {  // Si el número de elementos es par, leer un valor adicional y calcular la mediana como el promedio de los dos valores centrales
+                scanf("%d", &mediana2);
+                mediana= (mediana + mediana2) / 2;
+            }
+            break;
+        }
+    }
+
+    printf("La mediana es: %d", mediana);
+
+    return 0;
+}
+
+    return 0;
+}
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+		
 		break;
 		case 2:
 		printf("CARGA DE NUEVOS DATOS\n");
