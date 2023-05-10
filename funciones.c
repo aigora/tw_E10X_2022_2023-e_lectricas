d24f20ad3d95c68beb557454f2915ce962e5df2a
solomiya.havryshko <61050036+solomiyahav@users.noreply.github.com>
2023-05-10 21:44:48 +0200

    Update funciones.c
    cambio en la detección de coma como separador

--- funciones.c
+++ funciones.c
@@ -1,10 +1,11 @@
-//implementación de las funciones
+//implementaciÃ³n de las funciones
 #include<stdio.h>
 #include"funciones.h"
 #include "string.h"
 void menu(){
 		printf("1.Calculos Estadisticos\n2.Carga de nuevos datos\n3.Guardar resultados\n4.Busquedas\n5.Ordenacion de valores\n");
 }
+
 void guardar_datos_generacion_energias(tipos *datos){
 	 FILE *pf;
 	 int pos;
@@ -19,25 +20,25 @@ void guardar_datos_generacion_energias(tipos *datos){
   {
   	printf("Se ha abierto correctamente\n");
 int linea_actual = 0;
-    fseek(pf, 280, SEEK_SET);
+    fseek(pf, 413, SEEK_SET);
  pos = ftell(pf);
-    printf("La posición actual del puntero es %ld bytes.\n", pos);
+    printf("La posiciÃ³n actual del puntero es %ld bytes.\n", pos);
 while ( linea_actual <= 23) {
-	fscanf(pf,"%s %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f",
-        datos[linea_actual - 5].tipo_energia, &datos[linea_actual - 5].cant_generada[0],
-        &datos[linea_actual - 5].cant_generada[1], &datos[linea_actual - 5].cant_generada[2],
-        &datos[linea_actual - 5].cant_generada[3], &datos[linea_actual - 5].cant_generada[4],
-        &datos[linea_actual - 5].cant_generada[5], &datos[linea_actual - 5].cant_generada[6],
-        &datos[linea_actual - 5].cant_generada[7], &datos[linea_actual - 5].cant_generada[8],
-        &datos[linea_actual - 5].cant_generada[9], &datos[linea_actual - 5].cant_generada[10],
-        &datos[linea_actual - 5].cant_generada[11], &datos[linea_actual - 5].cant_generada[12],
-        &datos[linea_actual - 5].cant_generada[13], &datos[linea_actual - 5].cant_generada[14],
-        &datos[linea_actual - 5].cant_generada[15], &datos[linea_actual - 5].cant_generada[16],
-        &datos[linea_actual - 5].cant_generada[17], &datos[linea_actual - 5].cant_generada[18],
-        &datos[linea_actual - 5].cant_generada[19], &datos[linea_actual - 5].cant_generada[20],
-        &datos[linea_actual - 5].cant_generada[21], &datos[linea_actual - 5].cant_generada[22],
-        &datos[linea_actual - 5].cant_generada[23]);
-    // Leer los datos de la línea actual
+	fscanf(pf,"%[^,],%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f\n",
+        datos[linea_actual].tipo_energia, &datos[linea_actual].cant_generada[0],
+        &datos[linea_actual].cant_generada[1], &datos[linea_actual].cant_generada[2],
+        &datos[linea_actual].cant_generada[3], &datos[linea_actual].cant_generada[4],
+        &datos[linea_actual].cant_generada[5], &datos[linea_actual].cant_generada[6],
+        &datos[linea_actual].cant_generada[7], &datos[linea_actual].cant_generada[8],
+        &datos[linea_actual].cant_generada[9], &datos[linea_actual].cant_generada[10],
+        &datos[linea_actual].cant_generada[11], &datos[linea_actual].cant_generada[12],
+        &datos[linea_actual].cant_generada[13], &datos[linea_actual].cant_generada[14],
+        &datos[linea_actual].cant_generada[15], &datos[linea_actual].cant_generada[16],
+        &datos[linea_actual].cant_generada[17], &datos[linea_actual].cant_generada[18],
+        &datos[linea_actual].cant_generada[19], &datos[linea_actual].cant_generada[20],
+        &datos[linea_actual].cant_generada[21], &datos[linea_actual].cant_generada[22],
+        &datos[linea_actual].cant_generada[23]);
+    // Leer los datos de la lÃ­nea actual
     linea_actual++;
 }
 }
@@ -64,7 +65,7 @@ int linea_actual = 0;
  pos = ftell(pf);
     fseek(pf, 250, SEEK_SET);
  pos = ftell(pf);
-    printf("La posición actual del puntero es %ld bytes.\n", pos);
+    printf("La posiciÃ³n actual del puntero es %ld bytes.\n", pos);
     while ( linea_actual ==5) {
 	fscanf(pf,"%s %d/%d %d/%d %d/%d %d/%d %d/%d %d/%d %d/%d %d/%d %d/%d %d/%d %d/%d %d/%d %d/%d %d/%d %d/%d %d/%d %d/%d %d/%d %d/%d %d/%d %d/%d %d/%d %d/%d",titulo,&f[0].month,&f[0].year,&f[1].month,&f[1].year,&f[2].month,&f[2].year,&f[3].month,&f[3].year,
 	&f[4].month,&f[4].year,&f[5].month,&f[5].year,&f[6].month,&f[6].year,&f[7].month,&f[7].year,&f[8].month,&f[8].year,
