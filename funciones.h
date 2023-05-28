76880cd4d1cf8773cdd089ad1c95ea8ae28ce38e
Lauraantonadiaz <130753048+Lauraantonadiaz@users.noreply.github.com>
2023-05-28 17:21:27 +0200

    comentarios funciones.h

--- funciones.h
+++ funciones.h
@@ -12,28 +12,42 @@ void menu();
 void menu_calculos_estadisticos();
 void guardar_datos_generacion_energias(tipos*datos);
 void guardar_datos_generacion_fechas(fecha*f);
-float mediana(tipos *energias);
+
+
+
+//funciones para calculos anual y mensual 
+
 float mediana2(int m,tipos *energias);//funcion para calculo anual y mensual
 float media2(int m,tipos *energias);//funcion calculo anual y mensual
 float rango2(int m, tipos *energias);
 float moda2(int m, tipos *energias);
+float desviacion_tipica_2(int m, tipos *energias);
+float varianza2(int m, tipos *energias);
+
+
 void swap(float* n1, float* n2);
 void ordenar_vector(float vector[], int n);
 void guardar_fecha_tipos(fecha *fech,tipos *energias);//funcion asignacion fecha a la estructura tipos
+
+//funciones para calculos estadisticos globales
+
+float mediana(tipos *energias);
 float media(tipos *energias);
 float varianza(tipos *energias);
 float desviacion_tipica(tipos *energias);
 float moda_global(tipos*energias);
 float rango_global(tipos*energias);
+
+
+
 float valor_maximo(int filas,int columnas,int filas2,int columnas2,tipos *energias);
 float valor_minimo(int filas,int columnas,int filas2,int columnas2,tipos *energias);
 void ordenacion_maximo(int filas,tipos *energias); 
 void ordenacion_minimo(int filas,tipos *energias);
 void guardar_datos_generacion_energias(tipos* energias);
 void imprimir_datos_fichero();
-float desviacion_tipica_2(int m, tipos *energias);
-float varianza2(int m, tipos *energias);
+
 void buscar(char cadena[40],tipos *energia);
 void guardado_de_datos(char *tipo_calc,float datos,  char* archivo);
 int cerrar(int k);
-void vaciar_archivo(char* archivo);//se vacia el fichero al abrirlo en modo escritura y no realizar ninguna operación
+void vaciar_archivo(char* archivo);//se vacia el fichero al abrirlo en modo escritura y no realizar ninguna operaciÃ³n
